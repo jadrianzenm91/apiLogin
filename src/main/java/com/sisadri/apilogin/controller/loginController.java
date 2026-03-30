08256879a0f84f8aaef802580acacf37268af547
jonathan adrianzen manrique <14831563+jadrianzenm91@users.noreply.github.com>
2026-03-29 23:36:39 -0500

    implementacion de CorsConfig

--- src/main/java/com/sisadri/apilogin/controller/loginController.java
+++ src/main/java/com/sisadri/apilogin/controller/loginController.java
@@ -13,16 +13,13 @@ import com.sisadri.apilogin.model.Usuario;
 import com.sisadri.apilogin.service.LogueoService;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/auth")
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class loginController {
     
     @Autowired
